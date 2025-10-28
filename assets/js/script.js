139017df41431b39f3e35f89b0513999ad99829f
tonmoy227 <145573783+tonmoy227@users.noreply.github.com>
2025-10-28 18:40:22 +0600

    90%
    90%

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,6 +84,27 @@ Last change:    00/00/00
 	$('[data-background]').each(function() {
 		$(this).css('background-image', 'url('+ $(this).attr('data-background') + ')');
 	});
+
+
+	$('.marquee-left').marquee({
+		gap: 0,
+		speed: 40,
+		delayBeforeStart: 0,
+		direction: 'left',
+		duplicated: true,
+		pauseOnHover: true,
+		startVisible:true,
+	});
+	$('.marquee-right').marquee({
+		gap: 28,
+		speed: 40,
+		delayBeforeStart: 0,
+		direction: 'right',
+		duplicated: true,
+		pauseOnHover: true,
+		startVisible:true,
+	});
+
 	gsap.registerPlugin(ScrollTrigger);
 	
 	// Animation
@@ -220,6 +241,90 @@ Last change:    00/00/00
 		CaseStudy.forEach((wrapper, index) => animateCard([index], wrapper, index));
 	}
 
+	if ($('.lg-testi-slide').length > 0 ) {
+		var slider = new Swiper('.lg-testi-slide', {
+			slidesPerView: 5,
+			loop: true,
+			spaceBetween: 3,
+			speed: 1000,
+			navigation: {
+				nextEl: ".lg-testi-next",
+				prevEl: ".lg-testi-prev",
+			},
+			pagination: {
+				el: ".lg-testi-pagi",
+				clickable: true,
+			},
+			autoplay: {
+				enabled: true,
+				delay: 6000
+			},
+			breakpoints: {
+				'1600': {
+					slidesPerView: 5,
+				},
+				'1200': {
+					slidesPerView: 4,
+				},
+				'992': {
+					slidesPerView: 3,
+				},
+				'991': {
+					slidesPerView: 2,
+				},
+				'768': {
+					slidesPerView: 2,
+				},
+				'576': {
+					slidesPerView: 1,
+				},
+				'0': {
+					slidesPerView: 1,
+				},
+			},
+		});
+	};
 
+
+	if ($("#slider-range").length) {
+		$( "#slider-range" ).slider({
+			range: true,
+			min: 0,
+			max: 3000,
+			values: [ 0, 1500 ],
+			slide: function( event, ui ) {
+				$( "#amount" ).val( "" + ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+			}
+		});
+	};
+	if ($("#amount").length) {
+		$( "#amount" ).val( "" + $( "#slider-range" ).slider( "values", 0 ) +
+			" - " + $( "#slider-range" ).slider( "values", 1 ) );
+	};
+	$('.count').prop('disabled', true);
+	$(document).on('click','.plus',function(){
+		$('.count').val(parseInt($('.count').val()) + 1 );
+	});
+	$(document).on('click','.minus',function(){
+		$('.count').val(parseInt($('.count').val()) - 1 );
+		if ($('.count').val() == 0) {
+			$('.count').val(1);
+		}
+	});
+
+
+	if($('.ed-footer-1-menu').length) {
+		document.querySelector('.ed-footer-1-menu').addEventListener('click', function(event) {
+			const link = event.target.closest('li a');
+			if (!link) return;
+			event.preventDefault();
+			const dropdown = link.nextElementSibling;
+			if (!dropdown || !dropdown.classList.contains('has-dropdown')) return;
+			document.querySelectorAll('.has-dropdown').forEach(ul => {
+				if (ul !== dropdown) ul.style.display = 'none';
+			});
+			dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+		});
+	}
 
 })(jQuery);
